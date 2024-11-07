267494635c2c4424a4fabafdcc78951dacce0eec
Grigoriy Dyatlov <holy.crapicl@gmail.com>
2024-11-07 14:52:39 +0300

    initial commit

---
+++ src/main/java/ru/netology/Server/Main.java
@@ -0,0 +1,39 @@
+package ru.netology.Server;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+public class Main {
+    public static LinkedList<Server> serverList = new LinkedList<>(); // список всех нитей
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        ServerSocket server = new ServerSocket(port);
+        try {
+            while (true) {
+                Socket socket = server.accept();
+                try {
+                    serverList.add(new Server(socket));
+                } catch (IOException e) {
+                    socket.close();
+                }
+            }
+        } finally {
+            server.close();
+        }
+    }
+
+    public int readPort(String url) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(url))) {
+            return Integer.parseInt(br.readLine());
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return 0;
+    }
+}
